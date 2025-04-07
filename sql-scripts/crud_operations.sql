f3ccdff8bdf37c8482a01dd7cd971e7de1a4e42a
massan11 <mhassan.zoho@gmail.com>
2025-04-07 18:57:08 +0330

    Add SQL script to update user email by UserID

--- sql-scripts/crud_operations.sql
+++ sql-scripts/crud_operations.sql
@@ -4,3 +4,8 @@ VALUES ('John Doe', 'john@example.com', 'hashedpassword123');
 SELECT UserID, FullName, Email, CreatedAt FROM Users;
 
 SELECT UserID, FullName, Email, CreatedAt FROM Users WHERE UserID = 1;
+
+UPDATE Users 
+SET Email = 'newemail@example.com' 
+WHERE UserID = 1;
+
