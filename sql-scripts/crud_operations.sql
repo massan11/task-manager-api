62d08cd225096207f2b854f4ce6b8eabcc86f986
massan11 <mhassan.zoho@gmail.com>
2025-04-07 19:05:20 +0330

    Add SQL script to insert a new task associated with a project

--- sql-scripts/crud_operations.sql
+++ sql-scripts/crud_operations.sql
@@ -10,7 +10,7 @@ SET Email = 'newemail@example.com'
 WHERE UserID = 1;
 
 DELETE FROM Users WHERE UserID = 1;
-
+--------------------------------------------------------------------------
 INSERT INTO Projects (Name, Description, CreatedBy) 
 VALUES ('API Development', 'Building a REST API', 2);
 
@@ -23,5 +23,8 @@ SET Name = 'Updated Project Name'
 WHERE ProjectID = 1;
 
 DELETE FROM Projects WHERE ProjectID = 1;
+------------------------------------------------------------------------------------------
+INSERT INTO Tasks (Title, Description, Status, Priority, DueDate, ProjectID, AssignedTo) 
+VALUES ('Fix API Bug', 'Fix a critical issue in API', 'To Do', 'High', '2025-04-10', 3, 2);
 
 
